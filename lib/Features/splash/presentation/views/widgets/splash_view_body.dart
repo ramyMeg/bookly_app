8be455345402b5eeb98f9dfe001ac385f5a1755e
ramyMeg <ramymegall@gmail.com>
2025-04-13 22:34:52 +0200

    Splash ui done

---
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo)
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor ),
+      home: const SplashView(),
+    );
+  }
+}
