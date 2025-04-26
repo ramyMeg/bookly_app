eda235aefa7e8b1d6aa776e3692a67942a80cc48
ramyMeg <ramymegall@gmail.com>
2025-04-26 21:49:43 +0300

    Adding title & subtitle & rating to book details

--- lib/Features/home/widgets/featured_list_view.dart
+++ lib/Features/home/widgets/featured_list_view.dart
@@ -1,5 +1,5 @@
 
-import 'package:bookly_app/Features/home/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/Features/home/widgets/Custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -14,7 +14,7 @@ class FeaturedBooksListView extends StatelessWidget {
         itemBuilder: (context, index){
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 0),
-          child:  FeaturedListViewItem(),
+          child:  CustomBookImage(),
         );
       },),
     );
